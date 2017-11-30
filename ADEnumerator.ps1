3ecfd174dd6ba8a1f6a746954ace4e05befc7c12
Windex03 <31138950+Windex03@users.noreply.github.com>
2017-11-30 09:26:10 -0500

    Added a scroll effect and stop process
    Scroll added for user reference to give a visual that the script is running and added a kill command at the end to close once completed.

--- ADEnumerator.ps1
+++ ADEnumerator.ps1
@@ -4,6 +4,33 @@ Import-Module ActiveDirectory
 
 $filename = Read-Host "Enter Name for Output File: "
 
+ $scroll = "\|/"
+$idx = 0
+$job = Invoke-Command -ComputerName $env:ComputerName -ScriptBlock { Start-Sleep -Seconds 3 } -AsJob
+
+$origpos = $host.UI.RawUI.CursorPosition
+$origpos.Y += 1
+
+while ($job.State -eq "Running")
+{
+	$host.UI.RawUI.CursorPosition = $origpos
+	Write-Host $scroll[$idx] -NoNewline
+	$idx++
+	if ($idx -ge $scroll.Length)
+	{
+		$idx = 0
+	}
+	Start-Sleep -Seconds 1
+}
+
+# It's over - clear the activity indicator.
+$host.UI.RawUI.CursorPosition = $origpos
+Write-Host 'Compiling....Please Wait' 
+
 Get-ADUser -filter {Enabled -eq $true} -Properties whencreated, lastlogondate, PasswordLastSet | Where-Object{$_.DistinguishedName -like "*OU=*" } | select Name, WhenCreated, PasswordLastSet, LastLogonDate | Export-Csv -path "c:\temp\$filename.csv"
 
 echo "Results have been exported to c:\temp\$filename.csv"
+
+ Start-sleep -Seconds 10
+
+stop-process -Id $PID 
